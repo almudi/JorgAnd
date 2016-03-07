79f8b9f25602b209feb51f590a30782b828ec581
Jorge Desentre <jdesentre@gmail.com>
2016-03-07 14:53:41 +0100

    Chasing Enemy

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -8,6 +8,8 @@ public class Shot : MonoBehaviour {
 	public Transform bulletSpawn;
 	public float fireRate;
 
+	public Transform cursor;
+
 	ParticleSystem particle;
 	void Awake()
 	{
@@ -22,16 +24,35 @@ public class Shot : MonoBehaviour {
 	{
 		if (fireRate<=0) {
 			fireRate = 0.3f;
+			faceForward ();
 			particle = Instantiate (shotEffectPrefab, bulletSpawn.position, bulletSpawn.rotation) as ParticleSystem;
 			particle.Play ();
 			Instantiate (bullet, bulletSpawn.position, bulletSpawn.rotation);
 		}
 		fireRate -= Time.deltaTime;
-		Debug.Log ("Shot");
 	}
 
 	public ParticleSystem GetShot()
 	{
 		return shotEffectPrefab;
 	}
+
+	private void faceForward()
+	{
+		Camera camera = Camera.main;
+		Transform bulletSpawn = GetComponentInChildren<Transform> ();
+
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit))
+		{
+			Vector3 posDisparo = new Vector3 (floorHit.point.x, bulletSpawn.position.y, floorHit.point.z);
+			bulletSpawn.LookAt (posDisparo);
+
+			cursor.position = floorHit.point + Vector3.up;
+
+		}
+	}
+		
 }
---
+++ Assets/enemyScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyScript : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination (GameObject.FindGameObjectWithTag ("Player").transform.position);
+	}
+}
